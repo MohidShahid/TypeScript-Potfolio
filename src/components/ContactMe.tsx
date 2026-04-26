f6444c8214fe199068bb9ef074244b8e5ddc69bf
Mohid Shahid <your_email@example.com>
2026-04-26 17:41:43 +0500

    replace video's and change form fields background and border

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -43,13 +43,13 @@ const ContactMe = () => {
 
       <form className="lg:ml-auto space-y-4" action={"https://formspree.io/f/movglgwz"} method="POST">
         <input type='text' placeholder='Name'
-          className="w-full rounded-md py-3 px-4 bg-slate-100 text-slate-900 text-sm border border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent" />
+          className="w-full rounded-md py-3 px-4  text-slate-900 text-sm border-2 border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent" />
         <input type='email' placeholder='Email'
-          className="w-full rounded-md py-3 px-4 bg-slate-100 text-slate-900 text-sm border border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent" />
+          className="w-full rounded-md py-3 px-4  text-slate-900 text-sm border-2 border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent" />
         <input type='text' placeholder='Subject'
-          className="w-full rounded-md py-3 px-4 bg-slate-100 text-slate-900 text-sm border border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent" />
+          className="w-full rounded-md py-3 px-4  text-slate-900 text-sm border-2 border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent" />
         <textarea placeholder='Message' rows={6}
-          className="w-full rounded-md px-4 bg-slate-100 text-slate-900 text-sm pt-3 border border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent"></textarea>
+          className="w-full rounded-md px-4  text-slate-900 text-sm pt-3 border-2 border-gray-200 focus:border-slate-900 outline-none focus:bg-transparent"></textarea>
         <button type='button'
           className="text-white bg-slate-900 hover:bg-slate-800 tracking-wide rounded-md text-sm font-medium px-4 py-3 w-full cursor-pointer mt-2! border-0">Send message</button>
       </form>
